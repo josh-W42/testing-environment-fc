0e8fbee39fef66866098c13f71fb22331d24b88b
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-07-01 20:18:38 -0700

    first intial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # testing-environment-fc
-This is a testing environment for a website before deployment live.
+
+## Description
+This is a testing environment for a website before deployment live. Sharing this site with other memebers before deployment can be difficult, so I'd rather use this envirnement so live tests can be made.
+
+
+### Built With:
+* <a href="https://getbootstrap.com/">BootStrap</a>
+* <a href="https://fontawesome.com/">Font Awesome</a>
+* <a href="https://jquery.com/">jquery</a>
+* <a href="https://fonts.google.com/">Google Fonts</a>
+
+## Getting started:
+Head over to the <a href="https://josh-w42.github.io/testing-environment-fc/" />Github Pages site</a> to try it out. Then clone or fork this repository.
+
+### Authors
+Josh Wilson.
+
+### Acknowledgments:
+* Excited to work on this one.
---
+++ js/non-template-index.js
@@ -0,0 +1,127 @@
+(function () {
+  // Everything that follows should be primarily for the index.html page.
+  document.querySelector('.soundbutton').addEventListener('click', toggleSound);
+
+  // The code below is for implementing a 'lazy loading' strategy,
+  // a small place holder is over an image as source then, once the
+  // page loads it is replaced with the actual picture.
+  registerListener('load', setLazy);
+  registerListener('load', lazyLoad);
+  registerListener('scroll', lazyLoad);
+
+  let lazy = [];
+
+  function setLazy(){
+    lazy = document.getElementsByClassName('lazy');
+    console.log('Found ' + lazy.length + ' lazy images');
+  }
+
+  function lazyLoad(){
+    for(var i=0; i<lazy.length; i++){
+      if(isInViewport(lazy[i])){
+        if (lazy[i].getAttribute('data-src')){
+          lazy[i].src = lazy[i].getAttribute('data-src');
+          lazy[i].removeAttribute('data-src');
+        }
+      }
+    }
+  cleanLazy();
+  }
+
+  function cleanLazy(){
+    lazy = Array.prototype.filter.call(lazy, function(l){ return l.getAttribute('data-src');});
+  }
+
+  function isInViewport(el){
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function registerListener(event, func) {
+    if (window.addEventListener) {
+      window.addEventListener(event, func)
+    } else {
+      window.attachEvent('on' + event, func)
+    }
+  }
+  window.addEventListener('load', function () {
+    videoControl();
+    window.addEventListener('scroll', fadeInSection);
+
+    function videoControl() {
+      // This function will determine whether controls are visable for the main video.
+      // On mobile or tablet it gives users more options on how to deal with the video.
+      if (this.innerWidth < 787) {
+        document.querySelector('video').controls = true;
+        document.querySelector('video').loop = false;
+      }
+      else{
+        document.querySelector('video').controls = false;
+        document.querySelector('video').loop = true;
+      }
+    }
+
+    // I'm having content fade in when the user scrolls down to certain "target points."
+    // mainly for aestical reasons.
+
+    function fadeInSection() {
+      if(this.innerWidth > 1024) {
+        if (this.pageYOffset > (document.querySelector('#fadein-target-section').offsetHeight + 500)) {
+          document.querySelector('#section-main-content').style.display = "block";
+          this.removeEventListener('scroll', fadeInSection);
+          this.addEventListener('scroll', fadeInHighlight)
+        }
+      }
+      else if (this.innerWidth <= 1024) {
+        if (this.pageYOffset > (document.querySelector('#fadein-target-section').offsetHeight)) {
+          document.querySelector('#section-main-content').style.display = "block";
+          this.removeEventListener('scroll', fadeInSection);
+          this.addEventListener('scroll', fadeInHighlight)
+        }
+      }
+    }
+
+    function fadeInHighlight() {
+      if (this.innerWidth > 1024) {
+        if (this.pageYOffset > (document.querySelector('#fadein-target-highlight').offsetHeight + 500)) {
+          document.querySelector('#highlight-main-content').style.display = "block";
+          this.removeEventListener('scroll', fadeInHighlight)
+        }
+      }
+      else if (this.innerWidth > 575 && this.innerWidth <= 1024) {
+        if (this.pageYOffset > (document.querySelector('#fadein-target-highlight').offsetHeight)) {
+          document.querySelector('#highlight-main-content').style.display = "block";
+          this.removeEventListener('scroll', fadeInHighlight)
+        }
+      }
+      else if (this.innerWidth < 575) {
+        if (this.pageYOffset > (document.querySelector('#fadein-target-highlight').offsetHeight + 500)) {
+          document.querySelector('#highlight-main-content').style.display = "block";
+          this.removeEventListener('scroll', fadeInHighlight)
+        }
+      }
+    }
+  });
+
+  // This function will toggle the sound button for the main video.
+  // Note, on mobile and smaller tablet it will not be visible.
+
+  function toggleSound(e) {
+    e.target.classList.toggle('soundOn');
+    let video = document.querySelector('video');
+    if (video.muted === true) {
+      video.muted = false;
+      e.target.innerHTML = "Sound ON";
+    }
+    else if(video.muted === false) {
+      video.muted = true;
+      e.target.innerHTML = "Sound OFF";
+    }
+  }
+
+})();
