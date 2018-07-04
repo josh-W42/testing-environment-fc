9c937e04a52319e10a51f5ec5a6005c9de0b951e
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-07-03 17:23:25 -0700

    added changes to footer links and new fonts

--- js/non-template-about.js
+++ js/non-template-about.js
@@ -17,6 +17,53 @@ fArray.push(document.querySelector('#miyaSheker'));
 const fLimit = fArray.length;
 const limit = words.length;
 (function () {
+  // The code below is for implementing a 'lazy loading' strategy,
+  // a small place holder is over an image as source then, once the
+  // page loads it is replaced with the actual picture.
+  registerListener('load', setLazy);
+  registerListener('load', lazyLoad);
+  registerListener('scroll', lazyLoad);
+
+  let lazy = [];
+
+  function setLazy(){
+    lazy = document.getElementsByClassName('lazy');
+    console.log('Found ' + lazy.length + ' lazy images');
+  }
+
+  function lazyLoad(){
+    for(var i=0; i<lazy.length; i++){
+      if(isInViewport(lazy[i])){
+        if (lazy[i].getAttribute('data-src')){
+          lazy[i].src = lazy[i].getAttribute('data-src');
+          lazy[i].removeAttribute('data-src');
+        }
+      }
+    }
+  cleanLazy();
+  }
+
+  function cleanLazy(){
+    lazy = Array.prototype.filter.call(lazy, function(l){ return l.getAttribute('data-src');});
+  }
+
+  function isInViewport(el){
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.bottom >= 0 &&
+      rect.right >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function registerListener(event, func) {
+    if (window.addEventListener) {
+      window.addEventListener(event, func)
+    } else {
+      window.attachEvent('on' + event, func)
+    }
+  }
   window.addEventListener('load', function (){
     words.forEach(function (word){
       topValues.push(word.style.top);
