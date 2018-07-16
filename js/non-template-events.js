3088bbb9479693037683be0ba67b8c3393a3b86d
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-07-16 02:06:05 -0700

    set up events page with basic layout

--- js/non-template-events.js
+++ js/non-template-events.js
@@ -1,49 +1,156 @@
+
 (function () {
-  // The code below is for implementing a 'lazy loading' strategy,
-  // a small place holder is over an image as source then, once the
-  // page loads it is replaced with the actual picture.
-  registerListener('load', setLazy);
-  registerListener('load', lazyLoad);
-  registerListener('scroll', lazyLoad);
 
-  let lazy = [];
+  let data = {
+    lazy: [],
 
-  function setLazy(){
-    lazy = document.getElementsByClassName('lazy');
-    console.log('Found ' + lazy.length + ' lazy images');
-  }
+    cleanLazy: function() {
+      data.lazy = Array.prototype.filter.call(data.lazy, function(l){ return l.getAttribute('data-src');});
+    },
 
-  function lazyLoad(){
-    for(var i=0; i<lazy.length; i++){
-      if(isInViewport(lazy[i])){
-        if (lazy[i].getAttribute('data-src')){
-          lazy[i].src = lazy[i].getAttribute('data-src');
-          lazy[i].removeAttribute('data-src');
-        }
+    bannerID: 0,
+    bannerName: 'July Calendar',
+
+    banners: [
+      {
+        img: 'img/events/julyCalendar.png',
+        name: 'July Calendar'
+      },
+      {
+        img: 'img/events/augustCalendar.png',
+        name: 'August Calendar'
+      },
+      {
+        img: 'img/events/septemberCalendar.png',
+        name: 'September Calendar'
       }
-    }
-  cleanLazy();
-  }
+    ]
 
-  function cleanLazy(){
-    lazy = Array.prototype.filter.call(lazy, function(l){ return l.getAttribute('data-src');});
-  }
+  };
 
-  function isInViewport(el){
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.bottom >= 0 &&
-      rect.right >= 0 &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+  let model = {
 
-  function registerListener(event, func) {
-    if (window.addEventListener) {
-      window.addEventListener(event, func)
-    } else {
-      window.attachEvent('on' + event, func)
-    }
-  }
+      init: function() {
+          // The code below is for implementing a 'lazy loading' strategy,
+          // a small place holder is over an image as source then, once the
+          // page loads it is replaced with the actual picture.
+          model.registerListener('load', model.setLazy);
+          model.registerListener('load', model.lazyLoad);
+          model.registerListener('scroll', model.lazyLoad);
+
+          document.querySelector('#previousBanner').addEventListener('click', model.previousBanner);
+          document.querySelector('#nextBanner').addEventListener('click', model.nextBanner);
+      },
+
+
+      nextBanner: function() {
+        data.bannerID++;
+        if(data.bannerID >= data.banners.length) {
+          data.bannerID = 0;
+        }
+        data.bannerName = data.banners[data.bannerID];
+        model.bannerTransform('right');
+      },
+
+      bannerTransform: function(dir) {
+        if(dir === 'right'){
+          document.querySelector('#calendarImg').classList.add('flyOutRight');
+          document.querySelector('#calendarImg').classList.remove('flyInLeft');
+          setTimeout(function() {
+            document.querySelector('#calendarImg').classList.remove('flyOutRight');
+            document.querySelector('#calendarImg').classList.add('flyInRight');
+            connector.ready();
+          }, 750)
+        } else {
+          document.querySelector('#calendarImg').classList.add('flyOutLeft');
+          document.querySelector('#calendarImg').classList.remove('flyInRight');
+          setTimeout(function() {
+            document.querySelector('#calendarImg').classList.remove('flyOutLeft');
+            document.querySelector('#calendarImg').classList.add('flyInLeft');
+            connector.ready();
+          }, 750)
+        }
+      },
+
+      previousBanner: function() {
+        data.bannerID--;
+        if(data.bannerID < 0) {
+          data.bannerID = (data.banners.length - 1);
+        }
+        data.bannerName = data.banners[data.bannerID];
+        model.bannerTransform();
+      },
+
+      setLazy: function(){
+          data.lazy = document.querySelectorAll('.lazy');
+          console.log('Found ' + data.lazy.length + ' lazy images');
+      },
+
+      registerListener: function(event, func) {
+          if (window.addEventListener) {
+              window.addEventListener(event, func)
+          } else {
+              window.attachEvent('on' + event, func)
+          }
+      },
+
+      lazyLoad: function() {
+          for(var i = 0; i < data.lazy.length; i++){
+              if(model.isInViewport(data.lazy[i])){
+                  if (data.lazy[i].getAttribute('data-src')){
+                      data.lazy[i].src = data.lazy[i].getAttribute('data-src');
+                      data.lazy[i].removeAttribute('data-src');
+                  }
+              }
+          }
+          data.cleanLazy();
+      },
+
+      isInViewport: function(el){
+          var rect = el.getBoundingClientRect();
+          return (
+              rect.bottom >= 0 &&
+              rect.right >= 0 &&
+              rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+              rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+          );
+      }
+
+  };
+
+  let view = {
+
+      init: function() {
+
+      },
+
+
+      // this function is designed for the slideshow.
+
+      renderCalendar: function () {
+
+        let banner = data.banners[data.bannerID];
+
+        let bannerImg = document.querySelector('#calendarImg');
+        bannerImg.src = banner.img;
+        bannerImg.alt = banner.name;
+      },
+
+  };
+
+  let connector = {
+
+      init: function() {
+          model.init();
+          view.init();
+      },
+
+      ready: function() {
+        view.renderCalendar();
+      }
+
+
+  };
+
+  connector.init();
 })();
