ee97cbe1265711292e52be17bdf0adf5dd235ae0
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2018-08-29 01:31:20 -0700

    added in privacy notice for use of google analytics and added a link to all pages footer

---
+++ privacy/js/non-template-privacy.js
@@ -0,0 +1,77 @@
+(function () {
+
+  let data = {
+
+      lazy: [],
+      cleanLazy: function() {
+          data.lazy = Array.prototype.filter.call(data.lazy, function(l){ return l.getAttribute('data-src');});
+      },
+
+  };
+
+  let model = {
+
+      init: function() {
+          // The code below is for implementing a 'lazy loading' strategy,
+          // a small place holder is over an image as source then, once the
+          // page loads it is replaced with the actual picture.
+          model.registerListener('load', model.setLazy);
+          model.registerListener('load', model.lazyLoad);
+          model.registerListener('scroll', model.lazyLoad);
+
+      },
+
+      setLazy: function(){
+          data.lazy = document.querySelectorAll('.lazy');
+          console.log('Found ' + data.lazy.length + ' lazy images');
+      },
+
+      registerListener: function(event, func) {
+          if (window.addEventListener) {
+              window.addEventListener(event, func)
+          } else {
+              window.attachEvent('on' + event, func)
+          }
+      },
+
+      lazyLoad: function() {
+          for(var i=0; i < data.lazy.length; i++){
+              if(model.isInViewport(data.lazy[i])){
+                  if (data.lazy[i].getAttribute('data-src')){
+                      data.lazy[i].src = data.lazy[i].getAttribute('data-src');
+                      data.lazy[i].removeAttribute('data-src');
+                  }
+              }
+          }
+          data.cleanLazy();
+      },
+
+      isInViewport: function(el){
+          var rect = el.getBoundingClientRect();
+          return (
+              rect.bottom >= 0 &&
+              rect.right >= 0 &&
+              rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+              rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+          );
+      }
+  };
+
+  let view = {
+
+    init: function() {
+    },
+
+  };
+
+  let connector = {
+
+      init: function() {
+        model.init();
+        view.init();
+      },
+
+  };
+
+  connector.init();
+})();
