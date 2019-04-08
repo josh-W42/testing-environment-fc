09e2bb74efecc6f2ef985daf0e3cbe4575d4138b
Joshua-Daniel-Wilson <wilson.jd022@gmail.com>
2019-04-08 16:41:20 -0700

    Deleting old events and adding new ones

--- events/js/non-template-events.js
+++ events/js/non-template-events.js
@@ -15,188 +15,36 @@ import './fullcalendar-4.0.0-alpha.2/fullcalendar-4.0.0-alpha.2/fullcalendar.min
 
     events: [
       {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-02-05T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
+        title: 'FC Spring Team Application Drops',
+        start: '2019-03-30T18:00:00',
+        detail: `Want to learn how to dance in a friendly, supportive environment? Apply to join one of our spring training teams and follow your dreams! <br/><br/> Head over to our apply page to open the application when it drops!`,
+        imgSrc: "../img/Banners/springAuditions19.jpg",
         instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
+        eventLink: 'https://www.facebook.com/events/266512220895520/'
       },
       {
-        title: 'Psypher Mental Health Dance Workshops',
-        start: '2019-02-08T20:00:00',
-        detail: `Come out to Psypher's dance workshops!`,
-        imgSrc: "../img/events/Non_fc_events/psypher_workshops.jpg",
+        title: 'FC Spring Pre-Auditions Workshops',
+        start: '2019-04-02T18:00:00',
+        detail: `Nervous about dancing for the first time? Come to our pre-audition workshops to get a taste of what you’ll be learning on your teams, as well as meet our LSHIP members!`,
+        imgSrc: "../img/Banners/springAuditions19.jpg",
         instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/364316264191786/'
+        eventLink: 'https://www.facebook.com/events/266512220895520/'
       },
       {
-        title: 'FC Winter 2019 Workshop Series',
-        start: '2019-02-08T18:00:00',
-        detail: `It’s getting pretty cold outside, and we’ve got the perfect thing to help you warm up. Now presenting: FC Winter 2019 Workshop Series! Get ready to grow, train, and challenge yourself with a variety of different styles from choreographers of all backgrounds. It’s about to get hot in here...`,
-        imgSrc: "../img/events/workshops/winter/workshop_banner.jpg",
+        title: 'FC Spring Team Auditions Day 1',
+        start: '2019-04-03T18:00:00',
+        detail: `Want to learn how to dance in a friendly, supportive environment? Apply to join one of our spring training teams and follow your dreams!`,
+        imgSrc: "../img/Banners/springAuditions19.jpg",
         instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/332852180889489/'
+        eventLink: 'https://www.facebook.com/events/266512220895520/'
       },
       {
-        title: 'FC Winter 2019 Dodgeball Tournament',
-        start: '2019-02-09T14:00:00',
-        detail: `It’s time to pick back up that New Year’s resolution and start hitting the gym again, because it’s here: now presenting FC’s second ever Dodgeball Tournament! Have a taste for some friendly competition? Been hitting the gym since the new year for this very reason? It’s your time to shine! Sign up and battle it out with other members of our 2019 Winter training teams for a chance to win some awesome prizes!`,
-        imgSrc: "../img/events/dodgeball_0209.jpg",
+        title: 'FC Spring Team Auditions Day 2',
+        start: '2019-04-04T18:00:00',
+        detail: `Want to learn how to dance in a friendly, supportive environment? Apply to join one of our spring training teams and follow your dreams!`,
+        imgSrc: "../img/Banners/springAuditions19.jpg",
         instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/407300399841674/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-02-10T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
-      },
-      {
-        title: 'FC Winter 2019 Workshop Series',
-        start: '2019-02-15T20:00:00',
-        detail: `It’s getting pretty cold outside, and we’ve got the perfect thing to help you warm up. Now presenting: FC Winter 2019 Workshop Series! Get ready to grow, train, and challenge yourself with a variety of different styles from choreographers of all backgrounds. It’s about to get hot in here...`,
-        imgSrc: "../img/events/workshops/winter/workshop_banner.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/332852180889489/'
-      },
-      {
-        title: 'FC Winter Intensive 2019',
-        start: '2019-02-16T13:00:00',
-        detail: `Take a break from midterms and relieve all your pent up stress this President’s Day weekend in the best way possible. Join us for two jam-packed days of dancing! Learn from FOUR talented featured choreographers and build, train, and challenge yourself with FC.`,
-        imgSrc: "../img/events/wintensive_2019.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/294529687878927/'
-      },
-      {
-        title: 'FC Winter Intensive 2019',
-        start: '2019-02-17T13:00:00',
-        detail: `Take a break from midterms and relieve all your pent up stress this President’s Day weekend in the best way possible. Join us for two jam-packed days of dancing! Learn from FOUR talented featured choreographers and build, train, and challenge yourself with FC.`,
-        imgSrc: "../img/events/wintensive_2019.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/294529687878927/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-02-17T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
-      },
-      {
-        title: 'Psypher Mental Health Dance Workshops',
-        start: '2019-02-19T20:00:00',
-        detail: `Come out to Psypher's dance workshops!`,
-        imgSrc: "../img/events/Non_fc_events/psypher_workshops.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/364316264191786/'
-      },
-      {
-        title: 'FC Spring 2019 Dance LSHIP Info Session',
-        start: '2019-02-20T18:00:00',
-        detail: `Are you passionate about nurturing beginning dancers, and providing a safe and supportive environment for their growth? Foundations is looking for dedicated, positive, and responsible individuals to become captains and directors for our spring quarter teams! Considered it, but don’t know exactly what being dance L-SHIP entails? Curious about the application, time commitment, or experiences of past dance LSHIP? Come to our info Session on February 20th from 6-7PM to find all the answers!`,
-        imgSrc: "../img/events/danceLship_info.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/349755469203218/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-02-24T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
-      },
-      {
-        title: 'Samahang Modern Friday Night Workshops',
-        start: '2019-03-01T20:00:00',
-        detail: `Come out to workshops hosted by Samahang Modern`,
-        imgSrc: "../img/events/Non_fc_events/sama_winter_wksp.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/590586374718316/'
-      },
-      {
-        title: 'FC Winter 2019 Choreo Showcase',
-        start: '2019-03-01T19:00:00',
-        detail: `Creative juices spilling over? Overflowing with ideas from watching dance videos instead of studying? You sound like someone who NEEDS to apply for our 2019 Winter Choreo Showcase! Perform your own original choreo with friends (or alone!) in front of all of your peers! Have your moment in the spotlight and live out all of your creative fantasies.`,
-        imgSrc: "../img/events/winter_choreo.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/240872433462188/'
-      },
-      {
-        title: 'FC Winter 2019 Workshop Series',
-        start: '2019-03-02T20:00:00',
-        detail: `It’s getting pretty cold outside, and we’ve got the perfect thing to help you warm up. Now presenting: FC Winter 2019 Workshop Series! Get ready to grow, train, and challenge yourself with a variety of different styles from choreographers of all backgrounds. It’s about to get hot in here...`,
-        imgSrc: "../img/events/workshops/winter/workshop_banner.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/332852180889489/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-03-03T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
-      },
-      {
-        title: 'Psypher Mental Health Dance Workshops',
-        start: '2019-03-08T20:00:00',
-        detail: `Come out to Psypher's dance workshops!`,
-        imgSrc: "../img/events/Non_fc_events/psypher_workshops.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/364316264191786/'
-      },
-      {
-        title: 'Samahang Modern Friday Night Workshops',
-        start: '2019-03-08T20:00:00',
-        detail: `Come out to workshops hosted by Samahang Modern`,
-        imgSrc: "../img/events/Non_fc_events/sama_winter_wksp.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/590586374718316/'
-      },
-      {
-        title: 'FC Winter 2019 Workshop Series',
-        start: '2019-03-09T20:00:00',
-        detail: `It’s getting pretty cold outside, and we’ve got the perfect thing to help you warm up. Now presenting: FC Winter 2019 Workshop Series! Get ready to grow, train, and challenge yourself with a variety of different styles from choreographers of all backgrounds. It’s about to get hot in here...`,
-        imgSrc: "../img/events/workshops/winter/workshop_banner.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/332852180889489/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-03-10T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
-      },
-      {
-        title: 'FC Winter 2019 Workshop Series',
-        start: '2019-03-15T20:00:00',
-        detail: `It’s getting pretty cold outside, and we’ve got the perfect thing to help you warm up. Now presenting: FC Winter 2019 Workshop Series! Get ready to grow, train, and challenge yourself with a variety of different styles from choreographers of all backgrounds. It’s about to get hot in here...`,
-        imgSrc: "../img/events/workshops/winter/workshop_banner.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/332852180889489/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-03-17T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
-      },
-      {
-        title: 'NSUdays Winter Workshops',
-        start: '2019-03-24T17:00:00',
-        detail: `Come out to workshops hosted by NSU Modern!`,
-        imgSrc: "../img/events/Non_fc_events/nsu_sundays.jpg",
-        instructor_info: "",
-        eventLink: 'https://www.facebook.com/events/2189735164613122/'
+        eventLink: 'https://www.facebook.com/events/266512220895520/'
       },
     ],
 
@@ -215,7 +63,7 @@ import './fullcalendar-4.0.0-alpha.2/fullcalendar-4.0.0-alpha.2/fullcalendar.min
           document.addEventListener('load', connector.ready);
       },
 
-      setLazy: function(){
+      setLazy: function() {
           data.lazy = document.querySelectorAll('.lazy');
           console.log('Found ' + data.lazy.length + ' lazy images');
       },
